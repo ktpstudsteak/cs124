88a74783fb3a4fba90f6d68befab75c574009a80
Kaden <kadenpayne@gmail.com>
2019-10-25 00:16:47 -0600

    Created assign25

---
+++ assignments/assign25/assign25.cpp
@@ -0,0 +1,31 @@
+/***********************************************************************
+* Program:
+*    Assignment 25, Loop Design
+*    Brother Birch, CS 124
+* Author:
+*    Kaden Payne
+* Summary: 
+*    This program will use a function called 'displaytable()'
+*    to display a calendar on screen.
+*    Estimated:  0.75 hrs   
+*    Actual:     0.40 hrs
+*      Designing this function was hard. I had a hard time
+*      figuring out how to use loops to help me.
+************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+/**********************************************************************
+ * <MAIN DESCRIPTION HERE>
+ ***********************************************************************/
+int main()
+{
+   /*cout rules - ONLY KEEP IF NEEDED
+   cout.setf(ios::fixed);       //no sci not
+   cout.setf(ios::showpoint);   //always show decimal
+   cout.precision(2);           //always have 2 dec points.
+   */
+   
+   return 0;
+}
