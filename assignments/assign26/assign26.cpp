4e184a928156fee0f803d6e2b1859127ec173542
Kaden <kadenpayne@gmail.com>
2019-10-28 15:50:03 -0600

    created assign26

---
+++ assignments/assign26/assign26.cpp
@@ -0,0 +1,38 @@
+/***********************************************************************
+* Program:
+*    Assignment 26, Files
+*    Brother Birch, CS 124
+* Author:
+*    Kaden Payne
+* Summary: 
+*    This program will read 10 programs from a
+*    file and display the average.
+*    Estimated:  0.75 hrs   
+*    Actual:     0.20 hrs
+*      It took a while to understand and remember the syntax for 
+*      reading files.
+************************************************************************/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+float readFile(char fileName[])
+{
+    //open the file
+    ifstream fin(fileName); 
+    if(fin.fail)
+    {
+        return 
+    }
+}
+/**********************************************************************
+ * <MAIN DESCRIPTION HERE>
+ ***********************************************************************/
+int main()
+{
+   
+   
+   return 0;
+}
