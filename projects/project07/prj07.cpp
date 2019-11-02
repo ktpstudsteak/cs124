07be9dacc43920cccc1faf0dca7120fde3e44972
Kaden Payne <kadenpayne@gmail.com>
2019-11-02 13:47:12 -0600

    stub functions for pj07

--- projects/project07/prj07.cpp
+++ projects/project07/prj07.cpp
@@ -9,23 +9,119 @@
 *    on screen.
 *    Estimated:  0.75 hrs   
 *    Actual:     0.20 hrs
-*      <COMPLAIN ABOUT EMACS>
+*
 ************************************************************************/
 
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
+//Declare funtion prototypes
+int getMonth();
+int getYear();
+int numDaysInYear(int, int);
+int numDaysInMonth(int, int);
+int computeOffset(int, int);
+
+bool isLeapYear(int);
+
+void displayHeader(int, int);
+void displayTable(int, int);
+
 /**********************************************************************
- * <MAIN DESCRIPTION HERE>
+ * GET YEAR
+ * Prompt user for year
+ * Double check for 
+ ***********************************************************************/
+int getYear()
+{
+   return 0;
+}
+
+/**********************************************************************
+ * GET MONTH
+ * Prompt user for input and display output
+ ***********************************************************************/
+int getMonth()
+{
+   return 0; 
+}
+
+/**********************************************************************
+ * NUM DAYS IN YEAR
+ * Prompt user for input and display output
+ ***********************************************************************/
+int numDaysInYear(int month, int year)
+{
+   return 0;
+}
+
+/**********************************************************************
+ * IS LEAP YEAR
+ * Check if year is a leap year
+ ***********************************************************************/
+bool isLeapYear(int year)
+{
+   return 0;
+   /*
+
+   if (year % 4 != 0)
+    {
+        return false;
+    }
+
+    else if (year % 100 != 0)
+    {
+        return true;
+    }
+
+    else if (year % 400 != 0)
+    {
+        return false;
+    }
+
+    else
+    {
+        return true;
+    }
+
+    */
+}
+
+/**********************************************************************
+ * COMPUTE OFFSET
+ * Produce the offset for the first day of the month
+ ***********************************************************************/
+int computeOffset(int month, int year)
+{
+   return 0;
+}
+
+
+/**********************************************************************
+ * DISPLAY HEADER
+ * Display month and year over table
+ ***********************************************************************/
+ void displayHeader(int month, int year)
+ {
+
+ }rompt user for month and year
+
+/**********************************************************************
+ * DISPLAY TABLE
+ * Display the table with offset and correct mumber of days
+ ***********************************************************************/
+ void displayTable(int numDays, int offset)
+ {
+
+ }
+
+/**********************************************************************
+ * MAIN
+ * Prompt user for input and display output
  ***********************************************************************/
 int main()
 {
-   /*cout rules - ONLY KEEP IF NEEDED
-   cout.setf(ios::fixed);       //no sci not
-   cout.setf(ios::showpoint);   //always show decimal
-   cout.precision(2);           //always have 2 dec points.
-   */
-   
+    
    return 0;
 }
