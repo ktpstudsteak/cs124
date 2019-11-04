6f2b697fe053aad4c0708188334e15f78c876bc7
Kaden Payne <kadenpayne@gmail.com>
2019-11-03 22:05:01 -0700

    assign 30 done

--- assignments/assign30/assign30.cpp
+++ assignments/assign30/assign30.cpp
@@ -13,20 +13,35 @@
 ************************************************************************/
 
 #include <iostream>
+#include <cassert>
 using namespace std;
 
+const int TEN = 10;
 /**********************************************************************
  * GET GRADE
  * get the grades and add them to the array to be given to main
  ***********************************************************************/
-int getGrades(int grades[])
+int getGrades(int grades[], const int num)
 {
-   return 0;
+      for (int i = 0; i < num; i++)
+      {
+         cout << "Grade " << i + 1 << ": ";
+         cin  >> grades[i];
+      }
+
+      return;
+      
 }
 
-int averageGrades(int grades[])
+int averageGrades(const int grades[], const int num)
 {
-   return 0;
+   int sum = 0;
+   for (int i = 0; i < num; i++)
+   {
+      sum += grades[i];
+   }
+   
+   return sum / num;
 }
 /**********************************************************************
  * Main
@@ -34,7 +49,11 @@ int averageGrades(int grades[])
  ***********************************************************************/
 int main()
 {
-   int grades[10];
+   int grades[TEN];
    
+   getGrades(grades, TEN);
+   int avg = averageGrades(grades, TEN);
+
+   cout << "Average Grade: " << avg << "%\n";
    return 0;
 }
