1c0c2dacb477f224ad4f898d93e9b6a2acb1d6d3
Kaden Payne <kadenpayne@gmail.com>
2019-11-03 17:57:50 -0700

    stub functions for assign 30

---
+++ assignments/assign30/assign30.cpp
@@ -0,0 +1,40 @@
+/***********************************************************************
+* Program:
+*    Assignment 30, Array Syntax
+*    Brother Birch, CS 124
+* Author:
+*    Kaden Payne
+* Summary: 
+*    This program will get 10 grades and return the average
+*
+*    Estimated:  0.75 hrs   
+*    Actual:     0.20 hrs
+*
+************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+/**********************************************************************
+ * GET GRADE
+ * get the grades and add them to the array to be given to main
+ ***********************************************************************/
+int getGrades(int grades[])
+{
+   return 0;
+}
+
+int averageGrades(int grades[])
+{
+   return 0;
+}
+/**********************************************************************
+ * Main
+ * call the functions and pass array as a parameter to get average
+ ***********************************************************************/
+int main()
+{
+   int grades[10];
+   
+   return 0;
+}
