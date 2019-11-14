23de15ed7d9a5346836b66f33c6cb2982f638e67
Kaden Payne <kadenpayne@gmail.com>
2019-11-14 02:05:58 -0700

    project 09

--- projects/project09/pj09.cpp
+++ projects/project09/pj09.cpp
@@ -5,7 +5,7 @@
 * Author:
 *    Kaden Payne
 * Summary: 
-*    This program is a Mad Lib
+*    This program a few functions for the Mad Lib
 *
 *    Estimated:  0.75 hrs   
 *    Actual:     0.20 hrs
@@ -13,19 +13,118 @@
 ************************************************************************/
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
+//Define replacements
+#define WORD      32    //one word is 32 bits
+#define WORDS     256   //256 bits
+#define OPEN      ':'   //start
+#define ENDL      '!'   //newline
+#define PERIOD    '.'   //period
+#define COMMA     ','   //comma
+#define OPQUOTE   '<'   //open double quote
+#define CLQUOTE   '>'   //close double quote
+
+//Declare funtion prototypes
+void getFileName(char fileName[]);
+int  readData(char story[][WORD]);
+void processPrompt(char text[]);
 
 /**********************************************************************
- * <MAIN DESCRIPTION HERE>
+ * READ DATA
+ * prompt for filename, read file
+ ***********************************************************************/
+int readData()
+{
+   return 0;
+}
+
+/**********************************************************************
+ * MAIN
+ * will put everything together
  ***********************************************************************/
 int main()
 {
-   /*cout rules - ONLY KEEP IF NEEDED
-   cout.setf(ios::fixed);       //no sci not
-   cout.setf(ios::showpoint);   //always show decimal
-   cout.precision(2);           //always have 2 dec points.
-   */
+   char story[WORDS][WORD];
+
+   //read the story
+   int length = readData(story);
+
+   return 0;
+}
+
+/**********************************************************************
+ * GET FILE NAME
+ * get filename from user
+ ***********************************************************************/
+void getFileName(char fileName[])
+{
+   cout << "Please enter the filename of the Mad Lib: ";
+   cin >> fileName;
+   cin.ignore();
+   return;
+}
+
+/**********************************************************************
+ * READ DATA
+ * get filename, replace text, and return the operation
+ ***********************************************************************/
+int readData(char story[][WORD])
+{
+   char fileName[256];
+
+   //get file
+   getFileName(fileName);
+
+   //open file
+   ifstream fin(fileName);
+   if (fin.fail())
+   {
+      cout << "Unable to open file " << fileName << endl;
+      return 0;
+   }
+
+   //read all data and process keywords
+   int iWord = 0;
+   while (iWord < WORDS && fin >> story[iWord])
+   {
+      processPrompt(story[iWord]);
+      iWord++;
+   }
+
+   fin.close();
+   return iWord;
    
    return 0;
+}
+
+/**********************************************************************
+ * PROCESS PROMPT
+ * convert the prompt into the user's data inline
+ ***********************************************************************/
+void processPrompt(char text[])
+{
+   //check for open quotes
+   if (text[0] != OPEN || !isalpha(text[1]))
+   {
+      return;
+   }
+
+   //display the header
+   cout << "\t" << (char)toupper(text[1]);
+
+   //display body
+   for (char *p = text + 2 ; *p; p++)
+   {
+      cout << (*p == '_' ? ' ' : (char)tolower(*p));
+   }
+   
+   //display trailer
+   cout << ": ";
+   
+   //get user's input
+   cin.getline(text, WORD);
+
+   return;
 }
