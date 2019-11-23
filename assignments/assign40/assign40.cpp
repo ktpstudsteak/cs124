4f8fc946c31bbdff87028d9909dbd3d57958cf9d
Kaden Payne <kadenpayne@gmail.com>
2019-11-22 17:54:54 -0700

    assign 40 done

--- assignments/assign40/assign40.cpp
+++ assignments/assign40/assign40.cpp
@@ -15,50 +15,142 @@
 #include <fstream>
 #include <cassert>
 
+#define MAX 256
+
 //Funtion Prototypes
-char getFileName(char fileName[]);
-char readFile(char fileName[], char board[]);
-char processChars(char board[]);
-char display(char board[]);
-void saveFile();
+void getFileName(char fileName[]);
+void readFile(char fileName[], char board[][3]);
+//char processChars(char board[][3]);
+void display(char board[][3]);
+void saveFile(char board[][3]);
 
 using namespace std;
 
 /**********************************************************************
+ * main will put the other functions together
+ ***********************************************************************/
+int main()
+{
+   //prompt user for filename
+   char fileName[MAX];
+   getFileName(fileName);
+
+   char board[3][3];
+
+   readFile(fileName, board);
+   return 0;
+}
+
+/**********************************************************************
  * prompt user for filename
  ***********************************************************************/
-char getFileName(char fileName[])
+void getFileName(char * fileName)
 {
+   cout << "Please specify the file name: ";
+   cin  >> fileName;
    return;
 }
 
 /**********************************************************************
  * read the data from the file
  ***********************************************************************/
-char readFile(char fileName[], char board[])
+void readFile(char fileName[], char board[][3])
 {
+   ifstream fin(fileName);
+
+   //check fail
+   if (fin.fail())
+   {
+      cout << "Unable to open or read file " << fileName 
+           << ".\n";
+   }  
+
+   //Rows
+   int i = 0;
+
+   //Columns
+   int j = 0;
+
+   //For loop to replace chars
+   for (i = 0; i < 3; i++)
+   {
+      for (j = 0; j < 3; j++)
+      {
+         fin >> board[i][j];
+         if (board[i][j] == '.')
+         {
+            board[i][j] = ' ';
+         }
+      }
+   }
+   //Works becuase of prototypes 
+   display(board);
    return;
 }
 
 /**********************************************************************
  * process the trigger chars
  ***********************************************************************/
-char processChars(char board[])
+/*char processChars(char board[][3])
 {
    return;
 }
+*/
 /**********************************************************************
- * main will put the other functions together
+ * display the game
  ***********************************************************************/
-int main()
+void display(char board[][3])
 {
-   /*cout rules - ONLY KEEP IF NEEDED
-   cout.setf(ios::fixed);       //no sci not
-   cout.setf(ios::showpoint);   //always show decimal
-   cout.precision(2);           //always have 2 dec points.
-   */
-   
-
-
-   return 0;
+   int inc = 3;
+   for (int i = 0; i < 3; i++)
+   {
+      if (i != 0)
+      {
+         cout << "---+---+---" << endl;
+      }
+      for (int j = 0; j < 3; j++)
+      {
+         inc++;
+         cout << " " << board[i][j] << (inc % 3 == 0 ? " \n" : " |");
+      }
+   }
+   saveFile(board);
+   return;
 }
+
+/**********************************************************************
+ * prompt user to save the game
+ ***********************************************************************/
+void saveFile(char board[][3])
+{
+   char writeFileName[256];
+
+   cout << "Enter destination filename: ";
+   cin  >> writeFileName;
+
+   //output file
+   ofstream fout(writeFileName);
+   
+    //For loop to replace chars
+   for (int i = 0; i < 3; i++)
+   {
+      for (int j = 0; j < 3; j++)
+      {
+         if (board[i][j] != ' ')
+         {
+            fout << board[i][j] << " ";
+         }
+
+         else
+         {
+            board[i][j] = '.';
+            fout << board[i][j];
+         }
+         
+      }
+   }
+   
+   cout << "File written \n";
+   return;
+}
+
