bee24275c8976c2126e23ed4d2d1ee290d16ca30
Kaden Payne <kadenpayne@gmail.com>
2019-11-25 21:01:35 -0700

    Assign 42 created and done

---
+++ assignments/assign42/assign42.cpp
@@ -0,0 +1,29 @@
+/***********************************************************************
+* Program:
+*    Assignment 42, Dem Bones
+*    Brother Birch, CS 124
+* Author:
+*    Kaden Payne
+* Summary: 
+*    This program will generate the song "Dem Bones".
+*    Estimated:  0.75 hrs   
+*    Actual:     0.20 hrs
+*  
+************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+/**********************************************************************
+ * <MAIN DESCRIPTION HERE>
+ ***********************************************************************/
+int main()
+{
+   /*cout rules - ONLY KEEP IF NEEDED
+   cout.setf(ios::fixed);       //no sci not
+   cout.setf(ios::showpoint);   //always show decimal
+   cout.precision(2);           //always have 2 dec points.
+   */
+   
+   return 0;
+}
