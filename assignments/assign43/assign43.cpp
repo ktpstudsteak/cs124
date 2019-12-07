fa2475f4307a8119354da5a76e0b4c2fa5c5b474
Kaden Payne <kadenpayne@gmail.com>
2019-12-06 17:47:05 -0700

    assign43

---
+++ assignments/assign43/assign43.cpp
@@ -0,0 +1,30 @@
+/***********************************************************************
+* Program:
+*    Assignment 43, Income
+*    Brother Birch, CS 124
+* Author:
+*    Kaden Payne
+* Summary: 
+*    This program will convert feet to meter
+*
+*    Estimated:  0.75 hrs   
+*    Actual:     0.20 hrs
+*      
+************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/**********************************************************************
+ * Main will put other functions together.
+ ***********************************************************************/
+int main(int argc, char** argv)
+{
+   for (int i = 1; i < argc; i++)
+   {
+      cout << atof(argv[i]) << " feet is "
+           << atof (argv[i]) * .3048 << " meters" << endl;
+   }
+   return 0;
+}
